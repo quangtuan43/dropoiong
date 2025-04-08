377fb61228f05f12941ac2f5859fe76aa2c3ad96
mytranx <khanhnganhuin@gmail.com>
2025-04-08 23:25:59 +0700

    Daily update for

---
+++ drop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start date: February 4, 2025
+START_DATE="2025-01-04"
+# End date: February 28, 2025
+END_DATE="2025-01-28"
+
+# Convert start date to seconds since epoch
+CURRENT_DATE=$(date -d "$START_DATE" +%s)
+END_DATE_EPOCH=$(date -d "$END_DATE" +%s)
+
+while [ "$CURRENT_DATE" -le "$END_DATE_EPOCH" ]; do
+    # Format the current date
+    DATE_STR=$(date -d "@$CURRENT_DATE" "+%Y-%m-%d 12:00:00")
+
+    # Create a commit with a fake timestamp
+    GIT_COMMITTER_DATE="$DATE_STR" git commit --allow-empty --date="$DATE_STR" -m "Commit on $DATE_STR"
+
+    # Move to the next day
+    CURRENT_DATE=$((CURRENT_DATE + 86400))  # Add 1 day (86400 seconds)
+done
+
